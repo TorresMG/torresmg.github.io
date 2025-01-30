9e18f3897d82083c97b690221edf5a2f32ec7a4a
Michael Torres <139499918+TorresMG@users.noreply.github.com>
2025-01-30 17:19:29 -0500

    Delete assets directory

--- assets/javaCode/DriverD.java
+++
@@ -1,6 +0,0 @@
-// Testing the StudentData class
-public class DriverD {
-   public static void main(String[] args) {
-      new StudentData();
-   }
-}
--- assets/javaCode/DriverIn.java
+++
@@ -1,6 +0,0 @@
-// Testing the StudentData class
-public class DriverIn {
-   public static void main(String[] args) {
-      new Interest();
-   }
-}
